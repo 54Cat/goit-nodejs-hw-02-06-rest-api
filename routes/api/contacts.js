529e98dd504cd65e88073c24e45f62f29c84de12
Kateryna Hrabanova <KaterynaHrabanova@gmail.com>
2022-11-29 14:09:01 +0200

    add starts files

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,23 +3,23 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json({ message: 'get contacts' })
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json({ message: 'get contact by id' })
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json({ message: 'post new contact' })
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json({ message: 'delete contact' })
 })
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json({ message: 'update contact' })
 })
 
 module.exports = router

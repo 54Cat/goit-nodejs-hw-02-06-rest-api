681cf4a8e8bb17f4dbdabd2a4a42efc4d76a80b1
Kateryna Hrabanova <KaterynaHrabanova@gmail.com>
2022-12-10 18:05:56 +0200

    add start files

---
+++ controllers/contacts/getAllContacts.js
@@ -0,0 +1,8 @@
+const { Contact } = require("../../models/contacts")
+
+const getAll = async (req, res, next) => {
+  const result = await Contact.find({}, "-createdAt -updatedAt")
+  res.json(result)
+}
+
+module.exports = getAll
--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,53 @@
-// const fs = require('fs/promises')
+const { Schema, model } = require('mongoose');
+const Joi = require('joi')
+const { handleSchemaValidationErrors } = require('../helpers');
 
-const listContacts = async () => {}
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Set name for contact'],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+      unique: true, 
+      required: true,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true
+  }
+)
 
-const getContactById = async (contactId) => {}
+contactSchema.post("save", handleSchemaValidationErrors)
 
-const removeContact = async (contactId) => {}
+const addSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email(),
+  phone: Joi.string().required(),
+  favorite: Joi.boolean()
+})
 
-const addContact = async (body) => {}
+const updateFavoriteSchema = Joi.object({
+  favorite: Joi.boolean().required()
+})
 
-const updateContact = async (contactId, body) => {}
+const schemas = {
+  addSchema,
+  updateFavoriteSchema
+}
+
+const Contact = model("contact", contactSchema)
 
 module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
+  Contact,
+  schemas
 }

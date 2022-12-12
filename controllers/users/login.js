848a5b6460e2319f7aba3772836846082a4b7f1b
Kateryna Hrabanova <KaterynaHrabanova@gmail.com>
2022-12-12 19:53:57 +0200

    bugfix login

--- controllers/users/login.js
+++ controllers/users/login.js
@@ -8,20 +8,21 @@ const { SECRET_KEY } = process.env
 const login = async (req, res, next) => {
   const { email, password } = req.body
   const user = await User.findOne({ email })
-  const passwordCompare = bcrypt.compare(password, user.password)
-  console.log("email",email)
-  console.log("user.email",user.email)
-  console.log("user",user)
 
-  if (!user || !passwordCompare) {
+  if (!user || !(await bcrypt.compare(password, user.password))) {
     throw new Unauthorized (`Email or password is wrong`)
   }
-
   const payload = {
     id: user._id
   }
   const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "1h" })
-  res.status(201).json({token})
+  res.status(200).json({
+    token,
+    user: {
+      email: user.email,    
+      subscription: user.subscription       
+    }
+  })
 }
   
 module.exports = login
--- controllers/users/signup.js
+++ controllers/users/signup.js
@@ -1,18 +1,15 @@
-// const { Conflict } = require("http-errors")
 const bcrypt = require("bcryptjs")
-
 const { User } = require("../../models")
 
 const signup = async (req, res, next) => {
   const { email, password } = req.body
-  // const user = await User.findOne({ email })
-  // if (user) {
-  //   throw new Conflict (`User with ${email} already exist`)
-  // }
   const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+  const newUser = await User.create({ email, password: hashPassword })
 
-  const result = await User.create({ email, password: hashPassword })
-  res.status(201).json(result)
+  res.status(201).json({
+    email: newUser.email,
+    subscription: newUser.subscription
+  })  
 }
   
 module.exports = signup

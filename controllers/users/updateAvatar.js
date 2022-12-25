1213f53f06a9dc2facf0cf5d568890fdf1b2104d
Kateryna Hrabanova <KaterynaHrabanova@gmail.com>
2022-12-25 18:11:00 +0200

    add resize

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -1,6 +1,7 @@
 const fs = require("fs")
 const path = require("path")
 const { User } = require("../../models")
+const Jimp = require('jimp')
 
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars")
 
@@ -11,12 +12,17 @@ const updateAvatar = async (req, res) => {
     
     try {
         const resultUpload = path.join(avatarsDir, imageName)
-        fs.rename(tempUpload, resultUpload, () => {});
+        await fs.rename(tempUpload, resultUpload, () => {});
         const avatarURL = path.join("public", "avatars", originalname)
+
+        const image = await Jimp.read(resultUpload)
+        await image.resize(250, 250)
+        await image.writeAsync(`public/avatars/${imageName}`)
+
         await User.findByIdAndUpdate(req.user._id, { avatarURL })
         res.json({ avatarURL })
     } catch (error) {
-        fs.unlink(tempUpload, () => {})
+        await fs.unlink(tempUpload, () => {})
         throw error
     }
 }
